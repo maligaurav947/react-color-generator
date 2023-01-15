0e4c7aa686a445937ad37929634f008182c031b2
Mali Gaurav <maligaurav947@gmail.com>
2023-01-15 09:16:00 +0530

    V0.01

---
+++ src/components/ColorPalette.js
@@ -0,0 +1,47 @@
+function ColorPalette() {
+  function change() {
+    var color = Math.floor(Math.random() * 16777215).toString(16);
+    var hexcolor = "#" + color;
+    document.getElementById("radomcolorgenerator").style.backgroundColor =
+      hexcolor;
+
+    var color = Math.floor(Math.random() * 16777215).toString(16);
+    var hexcolor = "#" + color;
+    document.getElementById("radomcolorgenerator2").style.backgroundColor =
+      hexcolor;
+
+    var color = Math.floor(Math.random() * 16777215).toString(16);
+    var hexcolor = "#" + color;
+    document.getElementById("radomcolorgenerator3").style.backgroundColor =
+      hexcolor;
+
+    var color = Math.floor(Math.random() * 16777215).toString(16);
+    var hexcolor = "#" + color;
+    document.getElementById("radomcolorgenerator4").style.backgroundColor =
+      hexcolor;
+
+    var color = Math.floor(Math.random() * 16777215).toString(16);
+    var hexcolor = "#" + color;
+    document.getElementById("radomcolorgenerator5").style.backgroundColor =
+      hexcolor;
+  }
+  return (
+    <div className="max-h-full justify-center mb-2 mt-2 flex items-center flex-col gap-5">
+      <div className="flex items-center main-container">
+        <div id="radomcolorgenerator"></div>
+        <div id="radomcolorgenerator2"></div>
+        <div id="radomcolorgenerator3"></div>
+        <div id="radomcolorgenerator4"></div>
+        <div id="radomcolorgenerator5"></div>
+      </div>
+      <button
+        onClickCapture={change}
+        className="bg-slate-700 text-white p-2 rounded-md hover:bg-slate-300 hover:text-black"
+      >
+        Random Color
+      </button>
+    </div>
+  );
+}
+
+export default ColorPalette;
