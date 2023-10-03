d4c49f28f169163967abb592a9ae16d2673a122d
Mali Gaurav <maligaurav947@gmail.com>
2023-10-03 10:51:35 +0530

    -color Name

--- src/components/ColorPalette.js
+++ src/components/ColorPalette.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 
+
 function ColorPalette() {
-  const initialColors = ['#000', 'pink', 'red', 'white', 'yellow'];
+  const initialColors = ["#000", "pink", "red", "white", "yellow"];
   const [colors, setColors] = useState(initialColors);
 
   const randomizeColors = () => {
@@ -13,21 +14,28 @@ function ColorPalette() {
   };
 
   return (
-    <div className="max-h-full justify-center flex items-center flex-col gap-2">
-      <div className="flex items-center main-container">
+    <div className="max-h-full justify-center flex items-center flex-col">
+      <div className="grid grid-cols-1 lg:grid-cols-5">
         {colors.map((color, index) => (
-          <div
-            key={index}
-            style={{
-              backgroundColor: color,
-            }}
-            className="color-box"
-          ></div>
+          <div className="" style={{}}>
+            <div
+              key={index}
+              style={{
+                backgroundColor: color,
+              }}
+              className="h-[15vh] grid place-content-center w-[100vw] lg:w-[20vw] lg:h-[80vh]"
+            >
+              <div className="lg:hidden text-xl text-black">{color}</div>
+            </div>
+            <div className="lg:grid hidden place-content-center text-xl text-black">
+              {color}
+            </div>
+          </div>
         ))}
       </div>
       <button
         onClick={randomizeColors}
-        className="bg-slate-700 text-white mt-2 p-2 rounded-md hover:bg-slate-300 hover:text-black"
+        className="bg-slate-700 text-white mt-2 p-2 rounded-md hover:bg-slate-300 hover:text-black mt-5"
       >
         Random Color
       </button>
